fa68d39c6e53df510e3ca3ca7168b4a45d96f8f6
CourtJayne <65653978+CourtJayne@users.noreply.github.com>
2020-07-06 11:54:27 +0100

    Adding Files

---
+++ shop.js
@@ -0,0 +1,23 @@
+function showMessage(btnId){			
+        if(btnId=="btnShowMsg1")
+            alert("Weekend Tickets Cost 80 GBP");
+        else if(btnId=="btnShowMsg2")
+            alert("VIP Tickets Cost 100 GBP");
+        else if(btnId=="btnShowMsg3")
+            alert("Afterparty Tickets Cost 30 GPB");
+        else if(btnId=="btnShowMsg4")
+            alert("T-shirts cost 15 GBP")
+        else if(btnId=="btnShowMsg5")
+            alert("Hoodies cost 30 GBP")
+        else if(btnId=="btnShowMsg6")
+            alert("Hats cost 10 GBP")
+        else if(btnId=="btnShowMsg7")
+            alert("Lanyards cost 5 GBP")
+        else if(btnId=="btnShowMsg8")
+            alert("Wristbands cost 5 GBP")
+        else if(btnId=="btnShowMsg9")
+            alert("Keyrings cost 5 GBP")      
+        else	
+            alert("Invalid Button Id.");				
+    }
+
